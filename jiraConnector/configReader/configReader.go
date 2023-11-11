47e27b93211e329ce136802132fecd779cf5040e
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-11-11 20:57:49 +0300

    create struct ConfigReader (using viper)

--- jiraConnector/configReader/configReader.go
+++ jiraConnector/configReader/configReader.go
@@ -1,1 +1,19 @@
 package configreader
+
+import (
+	"github.com/spf13/viper"
+)
+
+type ConfigRaeder struct {
+	viperConfigReader *viper.Viper
+}
+
+func newConfigReader() *ConfigRaeder {
+	configReader := ConfigRaeder{}
+	configReader.viperConfigReader = viper.New()
+	configReader.viperConfigReader.SetConfigName("") //придумать имя конфига
+	configReader.viperConfigReader.SetConfigType("yaml")
+	configReader.viperConfigReader.AddConfigPath("") //соответственно добавить путь
+
+	return &configReader
+}
