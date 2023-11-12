f8255b58ac762cb332627700cf4745c78eda3699
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-11-12 22:29:47 +0300

    configReader start working

--- jiraConnector/configReader/configReader.go
+++ jiraConnector/configReader/configReader.go
@@ -1,19 +1,29 @@
 package configreader
 
 import (
+	"Jira-analyzer/jiraConnector/logger"
+
 	"github.com/spf13/viper"
 )
 
 type ConfigRaeder struct {
 	viperConfigReader *viper.Viper
+	viperLogger       *logger.JiraLogger
 }
 
-func newConfigReader() *ConfigRaeder {
+func CreateNewConfigReader() *ConfigRaeder {
 	configReader := ConfigRaeder{}
 	configReader.viperConfigReader = viper.New()
-	configReader.viperConfigReader.SetConfigName("config.yaml") //придумать имя конфига
+	configReader.viperConfigReader.SetConfigName("config") //придумать имя конфига
 	configReader.viperConfigReader.SetConfigType("yaml")
-	configReader.viperConfigReader.AddConfigPath("") //соответственно добавить путь
-
+	configReader.viperConfigReader.AddConfigPath("analyzer/configs") //соответственно добавить путь
+	viperLogger := logger.CreateNewLogger()
+	if err := configReader.viperConfigReader.ReadInConfig(); err != nil {
+		viperLogger.Log(logger.ERROR, err.Error())
+	}
 	return &configReader
+}
+
+func (configReader *ConfigRaeder) GetJiraUrl() string {
+	return configReader.viperConfigReader.GetString("jiraUrl")
 }
