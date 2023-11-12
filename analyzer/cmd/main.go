167f3377c2666e71a0d4c028c01a46aafd6eb4c3
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-11-12 22:31:47 +0300

    temp chages at main

--- analyzer/cmd/main.go
+++ analyzer/cmd/main.go
@@ -5,5 +5,5 @@ import "Jira-analyzer/jiraConnector/connector"
 func main() {
 	jiraConnector := connector.NewConnector()
 	jiraConnector.GetProjects()
-	jiraConnector.GetProjectInfo("AAR")
+	jiraConnector.GetProjectInfo("ACE")
 }
