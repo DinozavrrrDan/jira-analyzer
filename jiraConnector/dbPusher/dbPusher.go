c6f9b453ac49018f0d26709f02da59c3c81e6829
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-11-13 23:37:43 +0300

    dbPusher add a skeleton of him

--- jiraConnector/dbPusher/dbPusher.go
+++ jiraConnector/dbPusher/dbPusher.go
@@ -7,3 +7,34 @@ database/sql
 атомарность, то есть, если при скачивании части данных произошла
 ошибка, то никакие данные не будут записаны в БД (все или ничего)
 */
+
+import (
+	"Jira-analyzer/jiraConnector/configReader"
+	"Jira-analyzer/jiraConnector/logger"
+	"Jira-analyzer/jiraConnector/models"
+	"database/sql"
+)
+
+type DatabasePusher struct {
+	configReader *configReader.ConfigRaeder
+	logger       *logger.JiraLogger
+	database     *sql.DB
+}
+
+func CreateNewDatabasePusher() *DatabasePusher {
+	newReader := configReader.CreateNewConfigReader()
+	//то что нужно из логгера в бд (БОРЯ)
+	newDatabase, err := sql.Open("postgres", "")
+	if err != nil {
+
+	}
+	return &DatabasePusher{
+		configReader: newReader,
+		logger:       logger.CreateNewLogger(),
+		database:     newDatabase,
+	}
+}
+
+func (databasePusher *DatabasePusher) PushIssue(issues models.TransformedIssue) {
+
+}
