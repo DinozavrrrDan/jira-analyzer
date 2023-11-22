e11c7591ed172289368f93fcecd819cc1697a8a0
Недорезов Данил <nedorezov.dd@edu.spbstu.ru>
2023-11-22 20:20:06 +0300

    main proxy

--- analyzer/cmd/main.go
+++ analyzer/cmd/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"Jira-analyzer/jiraConnector/connector"
+	"Jira-analyzer/jiraConnector/apiServer"
 	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-	jiraConnector := connector.CreateNewJiraConnector()
-	projects := jiraConnector.GetProjects(5, 1, "")
-	fmt.Print(projects)
-	jiraConnector.GetProjectIssues("ACL")
+	apiServer := apiServer.CreateNewApiServer()
+	apiServer.StrartServer()
+
+	connectorTarget, err := url.Parse(fmt.Sprintf("http://%s:%d", "localhost", 8000))
+	if err != nil {
+		fmt.Printf("Error parsing target URL: %v\n", err)
+	}
+
+	gatewayMux := http.NewServeMux()
+	fmt.Println("Create proxy for connector server.")
+	gatewayMux.HandleFunc("/api/v1"+"/connector/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Recieved request for connector proxy: %s", r.URL.Path)
+		proxy := httputil.NewSingleHostReverseProxy(connectorTarget)
+		proxy.ServeHTTP(w, r)
+	})
+
 }
