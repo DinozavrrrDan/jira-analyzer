d4726b74446385963155f9e47555eb577a502e7d
boris <borissiobko@gmail.com>
2023-11-26 00:31:34 +0300

    resourceHandler && utils частично

--- analyzer/endpoints/resource/utils.go
+++ analyzer/endpoints/resource/utils.go
@@ -1,70 +1,66 @@
 package endpoints
 
 import (
+	"Jira-analyzer/analyzer/models"
 	"Jira-analyzer/common/logger"
-	"Jira-analyzer/jiraConnector/logger"
 
 	_ "github.com/lib/pq"
 )
 
-func (resourseHandler *ResourseHandler) GetIssueInfo(id int) IssueInfo {
-	issueInfo := IssueInfo{}
+func (resourseHandler *ResourceHandler) GetIssueInfo(id int) (models.IssueInfo, error) {
+	issueInfo := models.IssueInfo{}
 	var authorId, assigneeId int
 
 	if err := resourseHandler.database.QueryRow("id, projectId, authorId, assigneeId, key, summary, description, type, priority, createdTime, closedTime, updatedTime, timeSpent FROM issues where id = ?", id).Scan(&issueInfo.Id, &issueInfo.Project.Id, &authorId, &assigneeId, &issueInfo.Key, &issueInfo.Summary, &issueInfo.Description, &issueInfo.Type, &issueInfo.Priority, &issueInfo.Status, &issueInfo.CreatedTime, &issueInfo.ClosedTime, &issueInfo.UpdatedTime, &issueInfo.TimeSpent); err != nil {
-		resourseHandler.logger.Log(logger.ERROR, err.Error())
-		return issueInfo
+		return issueInfo, err
 	}
 
 	//еще достать автора и assignee
 
 	resourseHandler.logger.Log(logger.INFO, "GetIssueInfo successfully")
-	return issueInfo
+	return issueInfo, nil
 }
 
-func (resourseHandler *ResourseHandler) GetHistoryInfo(id int) HistoryInfo {
-	historyInfo := HistoryInfo{}
+func (resourceHandler *ResourceHandler) GetHistoryInfo(id int) (models.HistoryInfo, error) {
+	historyInfo := models.HistoryInfo{}
 
-	resourseHandler.logger.Log(logger.INFO, "GetHistoryInfo successfully")
-	return historyInfo
+	resourceHandler.logger.Log(logger.INFO, "GetHistoryInfo successfully")
+	return historyInfo, nil
 }
 
-func (resourseHandler *ResourseHandler) GetProjectInfo(id int) ProjectInfo {
-	projectInfo := ProjectInfo{}
+func (resourceHandler *ResourceHandler) GetProjectInfo(id int) (models.ProjectInfo, error) {
+	projectInfo := models.ProjectInfo{}
 
-	if err := resourseHandler.database.QueryRow("id, title FROM project where id = ?",
+	if err := resourceHandler.database.QueryRow("id, title FROM project where id = ?",
 		id).Scan(&projectInfo.Id, &projectInfo.Title); err != nil {
-		resourseHandler.logger.Log(logger.ERROR, err.Error())
-		return projectInfo
+		return projectInfo, err
 	}
 
-	resourseHandler.logger.Log(logger.INFO, "GetProjectInfo successfully")
-	return projectInfo
+	resourceHandler.logger.Log(logger.INFO, "GetProjectInfo successfully")
+	return projectInfo, nil
 }
 
-func (resourseHandler *ResourseHandler) InsertProject(projectInfo ProjectInfo) int {
+func (resourceHandler *ResourceHandler) InsertProject(projectInfo models.ProjectInfo) (int, error) {
 	var projectId int
 
-	if err := resourseHandler.database.QueryRow("INSERT INTO project (title) VALUES(?) RETURNING id",
+	if err := resourceHandler.database.QueryRow("INSERT INTO project (title) VALUES(?) RETURNING id",
 		projectInfo.Title).Scan(&projectId); err != nil {
-		resourseHandler.logger.Log(logger.ERROR, err.Error())
-		return projectId
+		return projectId, err
 	}
 
-	resourseHandler.logger.Log(logger.INFO, "InsertProject successfully")
-	return projectId
+	resourceHandler.logger.Log(logger.INFO, "InsertProject successfully")
+	return projectId, nil
 }
 
-func (resourseHandler *ResourseHandler) InsertIssue(issueInfo IssueInfo) int {
+func (resourseHandler *ResourceHandler) InsertIssue(issueInfo models.IssueInfo) (int, error) {
 	var issueId, authorId, assigneeId int
 
 	//достать authorId и assigneeId из таблицы author
 
 	if err := resourseHandler.database.QueryRow("INSERT INTO issues (projectId, authorId, assigneeId, key, summary, description, type, priority, status, createdTime, closedTime, updatedTime, timeSpent) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", issueInfo.Project.Id, authorId, assigneeId, issueInfo.Key, issueInfo.Summary, issueInfo.Description, issueInfo.Type, issueInfo.Priority, issueInfo.Status, issueInfo.CreatedTime, issueInfo.ClosedTime, issueInfo.UpdatedTime, issueInfo.TimeSpent).Scan(&issueId); err != nil {
-		resourseHandler.logger.Log(logger.ERROR, err.Error())
-		return issueId
+		return issueId, err
 	}
 
 	resourseHandler.logger.Log(logger.INFO, "InsertIssue successfully")
-	return issueId
+	return issueId, nil
 }
