c8afed1c119f6f1d6a46e8c936b7766e6131ed85
boris <borissiobko@gmail.com>
2023-12-05 21:25:07 +0300

    Забыл файлы добавить в пред коммите

--- analyzer/endpoints/analytic/handlers.go
+++ analyzer/endpoints/analytic/handlers.go
@@ -1,1 +1,58 @@
 package analytic
+
+import (
+	"Jira-analyzer/common/configReader"
+	"Jira-analyzer/common/logger"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+type AnalyticServer struct {
+	configReader *configReader.ConfigReader
+	logger       *logger.JiraLogger
+	database     *sql.DB
+}
+
+func CreateNewResourceServer() *AnalyticServer {
+	newReader := configReader.CreateNewConfigReader()
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		newReader.GetHostDB(),
+		newReader.GetPortDB(),
+		newReader.GetUserDb(),
+		newReader.GetPasswordDB(),
+		newReader.GetDatabaseName())
+	newDatabase, err := sql.Open("postgres", sqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	return &AnalyticServer{
+		configReader: newReader,
+		logger:       logger.CreateNewLogger(),
+		database:     newDatabase,
+	}
+}
+
+func (analyticServer *AnalyticServer) getGraph(responseWriter http.ResponseWriter, request *http.Request) {
+	projectId, err := strconv.Atoi(request.URL.Query().Get("project"))
+	if err != nil {
+		analyticServer.logger.Log(logger.ERROR, err.Error())
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	data, err := json.MarshalIndent(analyticServer.GraphFive(projectId), "", "\t")
+
+	_, err = responseWriter.Write(data)
+	responseWriter.WriteHeader(http.StatusOK)
+}
+
+func (analyticServer *AnalyticServer) handlers(router *mux.Router) {
+	router.HandleFunc(analyticServer.configReader.GetAnalyticPref()+"{group:[1-2]}", analyticServer.getGraph).
+		Queries("project", "{projectName}").
+		Methods("GET")
+}
--- analyzer/endpoints/analytic/utils.go
+++ analyzer/endpoints/analytic/utils.go
@@ -1,1 +1,45 @@
 package analytic
+
+import (
+	"Jira-analyzer/analyzer/models"
+	"Jira-analyzer/common/logger"
+	"log"
+)
+
+func (analyticServer *AnalyticServer) GraphFive(projectId int) []models.GraphData {
+
+	rows, err := analyticServer.database.Query(
+		"SELECT" +
+			" priority," +
+			" COUNT(*) AS task_count" +
+			" FROM issues" +
+			" GROUP BY priority" +
+			" CASE" +
+			" WHEN priority = 'Critical' THEN 1" +
+			" WHEN priority = 'Blocker' THEN 2" +
+			" WHEN priority = 'Major' THEN 3" +
+			" WHEN priority = 'Minor' THEN 4" +
+			" END;",
+	)
+
+	if err != nil {
+		analyticServer.logger.Log(logger.ERROR, err.Error())
+		return nil
+	}
+
+	var result []models.GraphData
+
+	for rows.Next() {
+		var entry models.GraphData
+
+		err := rows.Scan(&entry.PriorityType, &entry.Amount)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result = append(result, entry)
+	}
+
+	analyticServer.logger.Log(logger.INFO, "Successfully GraphFive")
+	return result
+}
